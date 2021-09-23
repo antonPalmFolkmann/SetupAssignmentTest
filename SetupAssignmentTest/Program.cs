ef4d713c0d0d5c935ed01a2dbad057798d715f6a
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-09-23 15:20:16 +0200

    Added folder structure.

---
+++ SetupAssignmentTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SetupAssignmentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
